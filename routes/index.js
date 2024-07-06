adeccc30b355c7ff63b59892328ad21252af1506
Kenny Cheng <kennycheng@comp.hkbu.edu.hk>
2024-07-06 11:42:52 +0800

    add post route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const mongoUri = 'mongodb://localhost:27017';
+
+let client = new MongoClient(mongoUri);
+let db = client['db'];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 
-  // connect to mongodb
+});
+
+router.post('/add', function(req, res, next) {
+  // connect to db
+  db.insertOne(req.body, (err, result) => {
+    if (err) {
+      res.send('error');
+    } else {
+      res.send('success');
+    }
+  });
 });
 
 module.exports = router;
