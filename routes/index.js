b255a7fcfcce560e9e609b68def24c7bc8872c49
Kenny Cheng <kennycheng@comp.hkbu.edu.hk>
2024-07-06 12:06:06 +0800

    update code

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb')
 const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017';
 
 let client = new MongoClient(mongoUri);
-let db = client.db('db').collection('sensordata')
+let db = client.db('db').collection('sensordata');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,14 +12,10 @@ router.get('/', function(req, res, next) {
 
 });
 
-router.post('/add', function(req, res, next) {
+router.post('/add', async function(req, res, next) {
   // insert to db
-  try {
-    let result = await db.insertOne(req.body);
-    return res.json(result);
-  } catch (e) {
-    return res.json(e);
-  }
+  let result = await db.insertOne(req.body);
+  return res.json(result);
 });
 
 module.exports = router;
